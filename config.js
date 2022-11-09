338723950be206bbbb961c105cf1d408bf969d14
RiO213 <74252771+lamtruong0213@users.noreply.github.com>
2022-11-09 20:59:50 +0700

    sinh nhat vui ve

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Chị';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = 'Gửi Din Din';
+const contentCard = 'Chúc chị  ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với chị  trong hôm nay và cả những ngày sau';
+const giftImage = 'MyAvatar.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
